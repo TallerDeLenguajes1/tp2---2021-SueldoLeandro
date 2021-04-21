a0e7db6375209c12e9210f750dee6660f36282a4
Sueldo Leandro <sueldoleandro2020@gmail.com>
2021-04-20 21:36:29 -0300

    Primer commit

---
+++ tp2_1_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define N 4
+#define M 5
+
+int main(void){
+
+
+int f,c;
+double mt[N][M] = {{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
+
+
+for(f = 0;f<N; f++)
+{
+        for(c = 0;c<M; c++)
+        {
+        printf("  %lf", mt[f][c]);
+
+        }
+    printf("\n");
+}
+
+getchar();
+    return 0;
+}
